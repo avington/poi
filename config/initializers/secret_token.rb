c6277bcd9e8a7910f38b2fc69b17fcb45aa26b2f
Rob Conery <rob@Anini.local>
2010-11-30 11:57:14 -1000

    Initial load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poi::Application.config.secret_token = '74f2e39073b6282614c4e510494b2ca0e1fd07a46cc140f7d9fb9423278dac459fae211c397ae3ed5f927e2da8c858a752422a5eb0468b76cb3f4115c2095bbe'
